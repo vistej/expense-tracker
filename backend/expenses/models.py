31339c403dc9319b3398bb9d41696c3a3907f874
vistej <teja.pvt@gmail.com>
2025-01-22 03:53:32 -0500

    added expense model

--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -7,3 +7,18 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='expenses')
+    item_name = models.CharField(max_length=100)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.item_name} - ${self.cost}"
+
+    class Meta:
+        ordering = ['-created_at']
