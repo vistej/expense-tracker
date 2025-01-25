1045266ea2de726efd18e8f0abcc922d75a12fd2
vistej <teja.pvt@gmail.com>
2025-01-25 14:19:52 -0500

    backend: added pagination

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 50,
 }
 
 SIMPLE_JWT = {
